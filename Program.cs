4c2d488f704ebfbf7ea92767c992ba194e856bb8
PRAKTIKAN <PRAKTIKAN@0605-PC-21>
2023-03-13 06:52:47 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Modul3_1302200091
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
