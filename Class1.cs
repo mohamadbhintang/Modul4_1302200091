a8d5c25eb097dd9cb832a4b246455fab3aeb5cd8
mohamadbhintang <101503536+mohamadbhintang@users.noreply.github.com>
2023-03-13 08:07:47 +0700

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul4_1302200091
+{
+    internal class KodeBuah
+    {
+        public enum Buah
+        {
+            Apel, Aprikot, Alpukat, Pisang, Paprika, Blackberry, Ceri, Kelapa, Jagung
+        }
+        public static string GetKodeBuah(Buah namaBuah)
+        {
+            string[] KodeBuah = { "A00", "B00", "C00", "D00", "E00", "F00", "H00", "I00", "J00" };
+
+            return KodeBuah[0];
+        }
+    }
+}
